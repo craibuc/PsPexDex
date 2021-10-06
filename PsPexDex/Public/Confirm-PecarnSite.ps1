c54f3c1a517e49ba4fea057f6a9c234503b9a17a
Craig Buchanan <craig.buchanan@cogniza.com>
2021-10-06 09:44:54 -0500

    initial implementation

---
+++ PsPexDex/Public/Confirm-PecarnSite.ps1
@@ -0,0 +1,43 @@
+<#
+.SYNOPSIS
+Registers 
+
+.PARAMETER siteid
+The four-letter, site or location id of the entity submitting data.
+
+.PARAMETER pin
+
+.PARAMETER publickey
+
+.EXAMPLE
+PS> Confirm-PecarnSite -siteid 'ABCD' -pin 123456 --publickey ''
+
+#>
+function Confirm-PecarnSite {
+
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [Parameter(Mandatory)]
+        [string]$siteid,
+        [Parameter(Mandatory)]
+        [string]$pin,
+        [Parameter(Mandatory)]
+        [string]$publickey
+    )
+    
+    $PexDexDirectory = $MyInvocation.MyCommand.Module.PrivateData.PexDexDirectory ? $MyInvocation.MyCommand.Module.PrivateData.PexDexDirectory : 'C:\Program Files\PEXDEX'
+    Write-Debug "PexDexDirectory: $PexDexDirectory"
+
+    Push-Location -Path $PexDexDirectory
+
+    $Command = "java ""-Dproperties.dir=C:\Program Files\PEXDEX"" -jar .\CLI\pexdexCLI.jar --confirmregister --siteid $siteid --pin $pin --publickey $publickey --spring.profiles.active=error"
+    Write-Debug "Command: $Command"
+
+    if ($PSCmdlet.ShouldProcess("$siteid/$email",'register'))
+    {
+        Invoke-Expression -Command $Command
+    }
+    
+    Pop-Location
+
+}
---
+++ Tests/Public/Confirm-PecarnSite.Tests.ps1
@@ -0,0 +1,112 @@
+BeforeAll {
+
+    # /Paths
+    $ProjectDirectory = Split-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -Parent
+    $PublicPath = Join-Path $ProjectDirectory "/PsPexDex/Public/"
+
+    $SUT = (Split-Path -Leaf $PSCommandPath) -replace '\.Tests\.', '.'
+
+    $Path = Join-Path $PublicPath $SUT
+    Write-Host "Path: $Path"
+
+    . (Join-Path $PublicPath $SUT)
+
+}
+
+Describe "Confirm-PecarnSite" {
+
+    Context "Parameter validation" {
+        BeforeAll {
+            $Command = Get-Command 'Confirm-PecarnSite'
+        }
+
+        Context "siteid" {
+            BeforeAll {
+                $ParameterName = 'siteid'
+            }
+
+            It "is a [string]" {
+                $Command | Should -HaveParameter $ParameterName -Type [string]
+            }
+            It "is mandatory" {
+                $Command | Should -HaveParameter $ParameterName -Mandatory
+            }
+        }
+
+        Context "pin" {
+            BeforeAll {
+                $ParameterName = 'pin'
+            }
+
+            It "is a [string]" {
+                $Command | Should -HaveParameter $ParameterName -Type [string]
+            }
+            It "is mandatory" {
+                $Command | Should -HaveParameter $ParameterName -Mandatory
+            }
+        }
+
+        Context "publickey" {
+            BeforeAll {
+                $ParameterName = 'publickey'
+            }
+
+            It "is a [string]" {
+                $Command | Should -HaveParameter $ParameterName -Type [string]
+            }
+            It "is mandatory" {
+                $Command | Should -HaveParameter $ParameterName -Mandatory
+            }
+        }
+    }
+
+    Context "Usage" {
+
+        BeforeAll {
+            # arrange
+            $Expected = @{
+                siteid='ABCD'
+                pin=123456
+                publickey='ssh-rsa 8edff50a-d0e7-49b5-aa43-3c5255a7cf50'
+                PexDexDirectory='C:\Program Files\PEXDEX'    
+            }
+        }
+
+        Context "When the mandatory parameters are supplied" {
+    
+            BeforeEach {
+                # arrange
+                Mock Push-Location
+                Mock Invoke-Expression
+                Mock Pop-Location
+
+                # act
+                Confirm-PecarnSite -siteid $Expected.siteid -pin $Expected.pin -publickey $Expected.publickey
+            }
+
+            It "changes to the directory that contains the PexDex EXE" {
+                # assert
+                Assert-MockCalled Push-Location -ParameterFilter {
+                    Write-Debug "Path: $Path"
+                    $Path -eq $Expected.PexDexDirectory
+                }
+            }
+
+            It "invokes PexDex's CLI with the expected values" {
+                # assert
+                Assert-MockCalled Invoke-Expression -ParameterFilter {
+                    Write-Debug "Command: $Command"
+
+                    $Command -like ("*--confirmregister --siteid {0} --pin {1} --publickey {2}*" -f $Expected.siteid, $Expected.pin, $Expected.publickey)
+                }
+            }
+
+            It "restores the original directory" {
+                # assert
+                Assert-MockCalled Pop-Location
+            }
+
+        }
+
+    }
+}
