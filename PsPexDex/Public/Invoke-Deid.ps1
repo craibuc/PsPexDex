6e3bb09d50f2fcd5d0305e1a1eb7ae24fe014bba
Craig Buchanan <craig.buchanan@cogniza.com>
2021-10-08 12:56:46 -0500

    initial commit

---
+++ PsPexDex/Public/Invoke-Deid.ps1
@@ -0,0 +1,66 @@
+<#
+.SYNOPSIS
+De-identify the XML file.
+
+.PARAMETER siteId
+The four-letter, site or location id of the entity submitting data.
+
+.PARAMETER submissionType
+0 = No Deid or 1 = Deid
+
+.PARAMETER xmlPath
+Path to the XML file.
+
+.PARAMETER pidPath
+Path to the PID file.
+
+.EXAMPLE
+PS> Invoke-Deid -siteId 'ABCD' -submissionType 0 -xmlPath \path\to\output\ABCD\ABCD_2020-03-04_to_2020-03-14.xml -pidPath \path\to\output\ABCD\PID_ABCD_2020-03-04_to_2020-03-14.txt
+
+#>
+function Invoke-Deid {
+
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [Parameter(Mandatory)]
+        [string]$siteId,
+
+        [Parameter(Mandatory)]
+        [ValidateSet(0,1)]
+        [int]$submissionType,
+
+        [Parameter(Mandatory)]
+        [string]$xmlPath,
+
+        [Parameter(Mandatory)]
+        [string]$pidPath
+    )
+    
+    Write-Debug "xmlPath: $xmlPath"
+    if ( (Test-Path -Path $xmlPath) -eq $false )
+    {
+        throw [System.IO.FileNotFoundException]::new("XML file not found", $xmlPath)
+    }
+
+    Write-Debug "pidPath: $pidPath"
+    if ( (Test-Path -Path $pidPath) -eq $false )
+    {
+        throw [System.IO.FileNotFoundException]::new("PID file not found", $pidPath)
+    }
+
+    $PexDexDirectory = $MyInvocation.MyCommand.Module.PrivateData.PexDexDirectory ? $MyInvocation.MyCommand.Module.PrivateData.PexDexDirectory : 'C:\Program Files\PEXDEX'
+    Write-Debug "PexDexDirectory: $PexDexDirectory"
+
+    Push-Location -Path $PexDexDirectory
+
+    $Command = "java ""-Dproperties.dir=C:\Program Files\PEXDEX"" -jar .\CLI\pexdexCLI.jar --spring.profiles.active=error --deidentify --siteid $siteid --submissionType $submissionType --file $xmlPath --pidtext $pidPath"
+    Write-Debug "Command: $Command"
+
+    if ($PSCmdlet.ShouldProcess("$siteid/$xmlPath",'de-identify'))
+    {
+        Invoke-Expression -Command $Command
+    }
+    
+    Pop-Location
+
+}
---
+++ Tests/Public/Invoke-Deid.Tests.ps1
@@ -0,0 +1,165 @@
+BeforeAll {
+
+    # /Paths
+    $ProjectDirectory = Split-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -Parent
+    $PublicPath = Join-Path $ProjectDirectory "/PsPexDex/Public/"
+
+    $SUT = (Split-Path -Leaf $PSCommandPath) -replace '\.Tests\.', '.'
+
+    $Path = Join-Path $PublicPath $SUT
+    Write-Host "Path: $Path"
+
+    . (Join-Path $PublicPath $SUT)
+
+}
+
+Describe "Invoke-Deid" {
+
+    Context "Parameter validation" {
+        BeforeAll {
+            $Command = Get-Command 'Invoke-Deid'
+        }
+
+        Context "siteid" {
+            BeforeAll {
+                $ParameterName = 'siteid'
+            }
+
+            It "is a [string]" {
+                $Command | Should -HaveParameter $ParameterName -Type [string]
+            }
+            It "is mandatory" {
+                $Command | Should -HaveParameter $ParameterName -Mandatory
+            }
+        }
+
+        Context "submissionType" {
+            BeforeAll {
+                $ParameterName = 'submissionType'
+            }
+
+            It "is a [int]" {
+                $Command | Should -HaveParameter $ParameterName -Type [int]
+            }
+            It "is mandatory" {
+                $Command | Should -HaveParameter $ParameterName -Mandatory
+            }
+            It "has a list valid values" -skip {
+            }
+        }
+
+        Context "xmlPath" {
+            BeforeAll {
+                $ParameterName = 'xmlPath'
+            }
+
+            It "is a [string]" {
+                $Command | Should -HaveParameter $ParameterName -Type [string]
+            }
+            It "is mandatory" {
+                $Command | Should -HaveParameter $ParameterName -Mandatory
+            }
+        }
+
+        Context "pidPath" {
+            BeforeAll {
+                $ParameterName = 'pidPath'
+            }
+
+            It "is a [string]" {
+                $Command | Should -HaveParameter $ParameterName -Type [string]
+            }
+            It "is mandatory" {
+                $Command | Should -HaveParameter $ParameterName -Mandatory
+            }
+        }
+    
+    }
+
+    Context "Usage" {
+
+        BeforeAll {
+            $xmlFile='ABCD_2020-03-04_to_2020-03-14.xml'
+            $xmlPath="TestDrive:\$xmlFile"
+            New-Item -Path $xmlPath -ItemType File
+
+            $pidFile='PID_ABCD_2020-03-04_to_2020-03-14.txt'
+            $pidPath="TestDrive:\$pidFile"
+            New-Item -Path $pidPath -ItemType File
+
+            # arrange
+            $Expected = @{
+                siteid='ABCD'
+                submissionType=0
+                xmlPath=$xmlPath
+                pidPath=$pidPath
+                PexDexDirectory='C:\Program Files\PEXDEX'    
+            }
+        }
+
+        Context "Valid" -skip {
+
+            Context "When the mandatory parameters are supplied" {
+        
+                BeforeEach {
+                    # arrange
+                    Mock Push-Location
+                    Mock Invoke-Expression
+                    Mock Pop-Location
+
+                    # act
+                    Invoke-Deid -siteid $Expected.siteid -submissionType $Expected.submissionType -xmlPath $Expected.xmlPath -pidPath $Expected.pidPath
+                }
+
+                It "changes to the directory that contains the PexDex EXE" {
+                    # assert
+                    Assert-MockCalled Push-Location -ParameterFilter {
+                        $Path -eq $Expected.PexDexDirectory
+                    }
+                }
+
+                It "invokes PexDex's CLI with the expected values" {
+                    # assert
+                    Assert-MockCalled Invoke-Expression -ParameterFilter {
+                        Write-Debug "Command: $Command"
+
+                        $Command -like ("*--deidentify --siteid {0} --submissionType {1} --file {2} --pidtext {3}*" -f $Expected.siteid, $Expected.submissionType, $Expected.xmlPath, $Expected.pidPath)
+                    }
+                }
+
+                It "restores the original directory" {
+                    # assert
+                    Assert-MockCalled Pop-Location
+                }
+
+            }
+
+        }
+
+        Context "Invalid" {
+
+            Context "When an invalid XML-file path is provided" {        
+                It "throws a file-not-found exception" {
+                    # arrange
+                    $xmlPath='\invalid\file\path'
+
+                    # act/assert
+                    { Invoke-Deid -siteid $Expected.siteid -submissionType $Expected.submissionType -xmlPath $xmlPath -pidPath $Expected.pidPath -ErrorAction Stop } | Should -Throw 'XML file not found'
+                }
+            }
+
+            Context "When an invalid PID-file path is provided" {        
+                It "throws a file-not-found exception" {
+                    # arrange
+                    $pidPath='\invalid\file\path'
+
+                    # act/assert
+                    { Invoke-Deid -siteid $Expected.siteid -submissionType $Expected.submissionType -xmlPath $Expected.xmlPath -pidPath $pidPath -ErrorAction Stop } | Should -Throw 'PID file not found'
+                }
+            }
+
+        }
+
+    }
+
+}
