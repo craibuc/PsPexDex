2a12694f5f921f71efc0b8d2f37ef54cc7e575fb
Craig Buchanan <craig.buchanan@choa.org>
2021-10-11 12:06:42 -0400

    help text revisions adding study parameter correcting $command

--- PsPexDex/Public/Submit-PecarnXmlFile.ps1
+++ PsPexDex/Public/Submit-PecarnXmlFile.ps1
@@ -1,15 +1,18 @@
 <#
 .SYNOPSIS
-Registers 
+Submits a PECARN, XML file to the DCC.
 
 .PARAMETER siteid
 The four-letter, site or location id of the entity submitting data.
 
 .PARAMETER xmlPath
-Path to the XML file.
+Path to the de-identified XML file.
+
+.PARAMETER study
+Valid values: pedscreen, registry.
 
 .EXAMPLE
-PS> Submit-PecarnXmlFile -siteid 'ABCD' -xmlPath \path\to\output\ABCD\ABCD_2020-03-04_to_2020-03-14.xml
+PS> Submit-PecarnXmlFile -siteid 'ABCD' -xmlPath \path\to\output\ABCD\ABCD_2020-03-04_to_2020-03-14.xml -study registry
 
 #>
 function Submit-PecarnXmlFile {
@@ -22,8 +25,9 @@ function Submit-PecarnXmlFile {
         [Parameter(Mandatory)]
         [string]$xmlPath,
 
-        [Parameter()]
-        [string]$type
+        [Parameter(Mandatory)]
+        [ValidateSet('pedscreen','registry')]
+        [string]$study
     )
     
     # if xmlPath doesn't exists, throw exception
@@ -37,7 +41,7 @@ function Submit-PecarnXmlFile {
 
     Push-Location -Path $PexDexDirectory
 
-    $Command = "java ""-Dproperties.dir=C:\Program Files\PEXDEX"" -jar .\CLI\pexdexCLI.jar --spring.profiles.active=error --validate --siteid $siteid --file $xmlPath"
+    $Command = "java ""-Dproperties.dir=C:\Program Files\PEXDEX"" -jar .\CLI\pexdexCLI.jar --spring.profiles.active=error --submit --siteid $siteid --file $xmlPath --study $study"
     Write-Debug "Command: $Command"
 
     if ($PSCmdlet.ShouldProcess("$siteid/$email",'register'))
